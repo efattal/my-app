c62d33ea092d57d1487de8b25fb37d854eb0e06c
e.fattal <e.fattal@lectra.com>
2020-05-22 23:40:14 +0200

    first commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<style>
+	h1 {
+		@apply bg-yellow-200
+	}
+</style>
+
+<h1 class="text-red-500">About this site</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
